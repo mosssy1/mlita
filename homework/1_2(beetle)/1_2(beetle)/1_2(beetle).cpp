39c250c3306c8c9998bffb1d05aead797b44b39f
mosssy1 <arina021003@gmail.com>
2025-09-19 17:57:44 +0300

    add lab

---
+++ homework/1_2(beetle)/1_2(beetle)/1_2(beetle).cpp
@@ -0,0 +1,128 @@
+﻿/*  1.2. Колорадский жук (4)
+Прямоугольный садовый участок длиной M ×N метров (1≤ M, N ≤5) разбит на квадраты со стороной 1 метр.
+На этом участке вскопана грядка картошки. Грядкой является связанное множество квадратов.  
+Иными словами, из любого квадрата этой грядки можно попасть в любой другой квадрат этой же грядки, 
+последовательно переходя по грядке из квадрата в квадрат через их общую сторону.
+В некоторых квадратах грядки растут кусты картошки. Колорадский жук прилетел на один из кустов. 
+Съев все листья, он переходит через общую сторону к какому-либо соседнему свежему кусту той же грядки.
+Жук не возвращается к тому кусту, на котором он уже был, и не приходит в те квадраты, где картошка
+не посажена. Определить наибольшее количество кустов картошки, которые сможет съесть колорадский жук.
+Ввод. В первой строке файла INPUT.TXT находятся числа M и N через пробел. Далее идут M строк по N 
+символов. Квадраты грядки с кустами картошки обозначаются символами '#', свободные квадраты – 
+символами '.'.
+Вывод. В единственной строке файла OUTPUT.TXT вывести одно число - наибольшее возможное количество
+съеденных кустов картошки.
+Пример 
+Ввод
+3 4
+####
+.#..
+.###
+Вывод
+7
+
+*/
+//Комиссарова Арина ПС-21
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <optional>
+
+using namespace std;
+
+const int MAX_N = 50;
+
+int dx[4] = { -1, 0, 1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int m, n;
+char garden[MAX_N][MAX_N];
+bool visited[MAX_N][MAX_N];
+int maxCount = 0;
+
+
+struct Args
+{
+    string inputFileName;
+    string outputFileName;
+};
+optional<Args> ParseArgs(int argc, char* argv[]);
+
+void Backward(int x, int y, int count) {
+    maxCount = max(maxCount, count);
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (nx >= 0 && nx < m && ny >= 0 && ny < n && garden[nx][ny] == '#' && !visited[nx][ny]) {
+            visited[nx][ny] = true;
+            count++;
+            Backward(nx, ny, count);
+            visited[nx][ny] = false;
+            count--;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+   auto args = ParseArgs(argc, argv);
+    if (!args)
+    {
+        return 1;
+    }
+
+    ifstream input;
+    input.open(args->inputFileName);
+
+    if (!input.is_open())
+    {
+        cout << "Failed to open '" << args->inputFileName << "' for reading\n";
+        return 1;
+    }
+
+    input >> m >> n;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            input >> garden[i][j];
+        }
+    }
+    input.close();
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (garden[i][j] == '#') {
+                visited[i][j] = true;
+                int count = 1;
+                Backward(i, j, count);
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    ofstream output;
+    output.open(args->outputFileName);
+
+    if (!output.is_open())
+    {
+        cout << "Failed to open '" << args->outputFileName << "' for writing\n";
+        return 1;
+    }
+    output << maxCount << endl;
+    output.close();
+
+    return 0;
+}
+
+optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Invalid arguments count\n";
+        return nullopt;
+    }
+
+    Args args;
+    args.inputFileName = argv[1];
+    args.outputFileName = argv[2];
+    return args;
+}
---
+++ homework/4_1(progression)/4_1(progression)/4_1(progression).cpp
@@ -0,0 +1,145 @@
+﻿/*Король Камбузии с детства боится несчастливых арифметических прогрессий с разностью 13. Однажды ему представили список расходов на нужды подданных, состоящий из N чисел. Король потребовал оставить только такую начальную часть списка, в которой не скрывается несчастливая арифметическая прогрессия. Либеральная общественность, считаясь с мнением короля, настаивает, тем не менее, на сохранении как можно большей части списка. Найти максимальное значение K такое, что из первых K чисел списка невозможно выделить M чисел, следующих в порядке их нахождения в списке и образующих последовательные члены несчастливой арифметической прогрессии. Выдать члены первой обнаруженной несчастливой прогрессии. 
+Ввод из файла INPUT.TXT. Первая строка содержит два целых положительных числа N и M, разделенных пробелом: N – количество чисел в списке, а M – недопустимое число членов прогрессии. Вторая строка содержит список расходов в виде целых положительных чисел. 
+Ограничения: 2 ≤ N, M ≤ 5000, 1 ≤ Xi ≤ 65000, время 1 с.
+Вывод в файл OUTPUT.TXT. В первой строке выводится единственное число K- максимальное количество начальных чисел списка, не содержащих в качестве подсписка M последовательных членов несчастливой арифметической прогрессии. Во второй строке выводятся через пробел члены первой обнаруженной несчастливой прогрессии. Если ее не обнаружено, вывести No.
+Пример 
+Ввод
+9 3
+5 9 3 22 16 19 35 7 29
+Вывод
+6
+9 22 35
+Комиссарова Арина ПС-21
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <optional>
+
+using namespace std;
+
+struct Args
+{
+    string inputFileName;
+    string outputFileName;
+};
+
+optional<Args> ParseArgs(int argc, char* argv[]);
+
+vector<int> FindUnluckyProgression(const vector<int>& nums, int M)
+{
+    int N = static_cast<int>(nums.size());
+    vector<vector<int>> dp(N, vector<int>(M, 0));
+
+    vector<int> result;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if (j == 0)
+            {
+                dp[i][j] = 1;
+            }
+            else if (i - j >= 0 && nums[i] - nums[i - 1] != nums[i - j] - nums[i - j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] = 1;
+            }
+
+            if (dp[i][j] == M)
+            {
+                for (int k = i - j; k <= i; k++)
+                {
+                    result.push_back(nums[k]);
+                }
+                return result;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+    auto args = ParseArgs(argc, argv);
+    if (!args)
+    {
+        return 1;
+    }
+
+    ifstream input(args->inputFileName);
+    if (!input.is_open())
+    {
+        cout << "Failed to open '" << args->inputFileName << "' for reading\n";
+        return 1;
+    }
+
+    ofstream output(args->outputFileName);
+    if (!output.is_open())
+    {
+        cout << "Failed to open '" << args->outputFileName << "' for writing\n";
+        return 1;
+    }
+
+    // чтение входных данных
+    int N, M;
+    input >> N >> M;
+    if (N < 2 || N > 5000 || M < 1 || M > N)
+    {
+        cout << "Invalid input values\n";
+        return 1;
+    }
+
+    vector<int> nums(N);
+    for (int i = 0; i < N; i++)
+    {
+        input >> nums[i];
+        if (nums[i] < 1 || nums[i] > 65000)
+        {
+            cout << "Invalid input values\n";
+            return 1;
+        }
+    }
+    input.close();
+
+    // поиск несчастливой прогрессии
+    vector<int> unluckyProgression = FindUnluckyProgression(nums, M);
+
+    // вывод результатов
+    if (unluckyProgression.empty())
+    {
+        output << "No" << endl;
+    }
+    else
+    {
+        output << unluckyProgression.size() << endl;
+        for (int i = 0; i < unluckyProgression.size(); i++)
+        {
+            output << unluckyProgression[i] << " ";
+        }
+        output << endl;
+    }
+    output.close();
+
+    return 0;
+}
+
+
+optional<Args> ParseArgs(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        cout << "Invalid arguments count\n";
+        return nullopt;
+    }
+
+    Args args;
+    args.inputFileName = argv[1];
+    args.outputFileName = argv[2];
+    return args;
+}
